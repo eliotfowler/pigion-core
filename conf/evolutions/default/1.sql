37fa2b4c98081c82dbb7bdf1fa9df674fa101e5f
Eliot Fowler <eliot@eliotfowler.com>
2014-02-09 11:10:47 -0500

    db configuration

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,15 @@
+# Destination schema
+
+# --- !Ups
+
+CREATE SEQUENCE destination_id_seq;
+CREATE TABLE destination (
+  id integer NOT NULL DEFAULT nextval('destination_id_seq'),
+  originalUrl text NOT NULL,
+  shortUrlHash text NOT NULL
+);
+
+# --- !Downs
+
+DROP TABLE destination;
+DROP SEQUENCE destination_id_seq;
