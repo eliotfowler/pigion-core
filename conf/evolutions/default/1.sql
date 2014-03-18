24f835e1f2b6b8e20cae6a90704ed7c94eddba26
Eliot Fowler <eliot@eliotfowler.com>
2014-03-17 21:38:02 -0400

    adding user tables

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -11,7 +11,38 @@ CREATE TABLE destination (
   contentType text NOT NULL
 );
 
+CREATE TABLE p_user (
+  userId text NOT NULL,
+  providerId text NOT NULL,
+  firstName text,
+  lastName text,
+  fullName text,
+  email text,
+  oAuth1Token text,
+  oAuth1Secret text,
+  oAuth2AccessToken text,
+  oAuth2TokenType text,
+  oAuth2ExpiresIn integer,
+  oAuth2RefreshToken text,
+  avatarUrl text,
+  passwordHasher text,
+  password text,
+  passwordSalt text,
+  authenticationMethod text
+);
+
+CREATE TABLE token (
+  uuid text NOT NULL,
+  email text NOT NULL,
+  creationTime timestamp,
+  expirationTime timestamp,
+  isSignUp boolean
+);
+
 # --- !Downs
 
 DROP TABLE destination;
 DROP SEQUENCE destination_id_seq;
+
+DROP TABLE user;
+DROP TABLE token;
