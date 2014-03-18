8181073177e02ea35f175d86b77e8c3b2c8124c6
Eliot Fowler <eliot@eliotfowler.com>
2014-03-17 21:36:20 -0400

    securing destinations

--- app/controllers/Destinations.scala
+++ app/controllers/Destinations.scala
@@ -1,30 +1,19 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 import models.Destination
 import play.api.libs.json._
-import java.net.{URLEncoder, URL}
 import play.api.data._
 import play.api.data.Forms._
-import fly.play.s3.{BucketFile, S3}
-import scala.concurrent.{ExecutionContext, Await}
-import scala.concurrent.duration.Duration
-import ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import scala.concurrent.Await
-import scala.concurrent.Awaitable
-import com.sksamuel.scrimage.Image
-import com.sksamuel.scrimage.ScaleMethod.Bicubic
-import java.io.{File, OutputStream}
+import securesocial.core._
 
-object Destinations extends Controller {
+object Destinations extends Controller with SecureSocial {
 
   case class UrlData(originalUrl: String)
 
   val urlForm = Form(
     mapping(
-      "originalurl" -> text//.verifying(!_.startsWith("javascript")).verifying(!_.startsWith("data"))
+      "originalurl" -> text
     )(UrlData.apply)(UrlData.unapply)
   )
 
@@ -34,10 +23,8 @@ object Destinations extends Controller {
     )
   )
 
-  def shortenUrl = Action { implicit request =>
-    Logger.info("request body is " + request.body.toString)
+  def shortenUrl = SecuredAction { implicit request =>
     val originalUrl: String = (request.body.asJson.get \ "originalUrl").asOpt[String].get
-    val url: URL = new URL(originalUrl)
     Ok(Json.obj(
       "originalUrl"->originalUrl,
       "shortUrl"->Destination.create(originalUrl, originalUrl, "url")
@@ -55,35 +42,7 @@ object Destinations extends Controller {
     }
   }
 
-  def getImageFromS3(destination: Destination) = Action.async {
-  val bucket = S3("pigion")
-  val result = bucket get URLEncoder.encode(destination.fileName, "UTF-8")
-  result.map {
-    case BucketFile(name, contentType, content, acl, headers) => {
-      Ok(Image(content).bound(250,250).write)
-    }
-    case _ => BadRequest("Unable to retrieve file.")
-  }
-}
-
-
-  def displayContentFromKey(key: String) = Action.async {
-    val destination = Destination.getDestinationForHash(key).get
-    val bucket = S3("pigion")
-    val result = bucket get URLEncoder.encode(destination.fileName, "UTF-8")
-    result.map {
-      case BucketFile(name, contentType, content, acl, headers) => {
-        val out:File = null
-
-//        Ok(views.html.imageViewer(Image(content).bound(250,250).write))
-        Ok("test")
-      }
-      case _ => BadRequest("Unable to retrieve file.")
-    }
-  }
-
-  def all = Action {
+  def all = SecuredAction {
     Ok(Json.toJson(Destination.all()))
   }
-
 }
