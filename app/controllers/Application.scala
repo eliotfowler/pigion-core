4191ff46bc58176b05218104d1bd0b876f40b660
Eliot Fowler <eliot@eliotfowler.com>
2014-03-17 23:08:09 -0400

    limiting app to beta testers only

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,15 @@
 package controllers
 
+import _root_.java.util.UUID
 import play.api._
 import play.api.mvc._
 import securesocial.core._
+import securesocial.core.providers._
+import securesocial.core.providers.utils.Mailer
+import org.joda.time.DateTime
 
 object Application extends Controller with SecureSocial {
+  val betaInviterKey = System.getenv("BETA_INVITER_KEY")
 
   def index = SecuredAction {
     Ok(views.html.index())
@@ -14,6 +19,28 @@ object Application extends Controller with SecureSocial {
     Ok(views.html.signupBetaClosed())
   }
 
-  def sendBetaInvite(email: String) = TODO
+  def sendBetaInvite(email: String, key: String)= Action { implicit request =>
+    if(key equals betaInviterKey) {
+      val token = createToken(email, isSignUp = true)
+      Mailer.sendSignUpEmail(email, token._1)
+      Ok
+    } else {
+      BadRequest
+    }
+  }
+
+  private def createToken(email: String, isSignUp: Boolean): (String, Token) = {
+    val uuid = UUID.randomUUID().toString
+    val now = DateTime.now
+
+    val token = Token(
+      uuid, email,
+      now,
+      now.plusMinutes(10080),
+      isSignUp = isSignUp
+    )
+    UserService.save(token)
+    (uuid, token)
+  }
 
 }
